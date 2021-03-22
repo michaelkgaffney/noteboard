fce37d92698ed25819612e52ba7c64e962f35595
Michael Gaffney <michaelgaffney@gMichaelGaffney.local>
2021-03-22 13:50:38 -0500

    Initial creation

---
+++ src/main/java/com/galavanize/demos/noteboard/NoteboardApplication.java
@@ -0,0 +1,13 @@
+package com.galavanize.demos.noteboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoteboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoteboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/galavanize/demos/noteboard/NoteboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.galavanize.demos.noteboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoteboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
